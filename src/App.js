44b55bc353101a6e0028311ef640b5b75ebf088e
SilverFlame83 <imren_83@abv.bg>
2022-12-25 17:23:09 +0200

    useEfect implemented

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
 import cinemaImg from "./assests/cinema.jpg";
@@ -9,12 +9,12 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  async function movieHandler() {
+  const movieHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
 
     try {
-      const response = await fetch("https://swapi.dev/api/film");
+      const response = await fetch("https://swapi.dev/api/films");
 
       if (!response.ok) {
         throw new Error("Something went wrong!");
@@ -35,7 +35,11 @@ function App() {
     }
 
     setIsLoading(false);
-  }
+  },[]);
+
+  useEffect(() => {
+    movieHandler();
+  }, [movieHandler]);
   let content = <p>No movies!</p>;
 
   if (movie.length > 0) {
